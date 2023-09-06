1396d407cd288501cc789c3235e48b039884abfe
ALUNO K9 <alunok9@acad.unifor.br>
2023-09-06 10:39:25 -0300

    varias mudanças

--- Main.gd
+++ Main.gd
@@ -53,7 +53,7 @@ func _on_Player2_pontua2():
 		Score2 += 1
 		$ponto.play()
 		$placar2.text = str(Score2)
-	if Score2 >= 5:
+	if Score2 >= 1:
 		$Button.show()
 		$tema.stop()
 		$resultado.text = "Player 2 Ganhou"
--- Menu.gd
+++ Menu.gd
@@ -3,8 +3,9 @@ extends Node
 
 
 
-func _ready():
-	pass 
+func _process(delta):
+	if Input.is_action_pressed("enter"):
+		get_tree().change_scene("res://Main.tscn")
 
 
 
